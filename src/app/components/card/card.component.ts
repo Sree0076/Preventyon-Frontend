e8335ba483d776c9596b5dbbc35c6818ce0e0f87
Jomi Susan Mathew <jomisusanmathew@gmail.com>
2024-06-30 15:00:52 +0530

    feat: dynamically passed card contents

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,23 +1,37 @@
+import { NgClass } from '@angular/common';
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() privacyIncidentTotalCount!: number;
-  @Input() privacyIncidentPending!: number;
-  @Input() privacyIncidentClosed!: number;
+  @Input() title!: string;
+  @Input() totalCount!: number;
+  @Input() pendingCount!: number;
+  @Input() closedCount!: number;
+  @Input() iconSvg!: string;
+  @Input() cardClass!: string;
 
-  @Input() securityIncidentTotalCount!: number;
-  @Input() securityIncidentPending!: number;
-  @Input() securityIncidentClosed!: number;
+  @Input() color1!: string;
+  @Input() color2!: string;
+  @Input() color1Percentage!: string;
+  @Input() color2Percentage!: string;
 
-  @Input() qualityIncidentTotalCount!: number;
-  @Input() qualityIncidentPending!: number;
-  @Input() qualityIncidentClosed!: number;
+  sanitizedIconSvg!: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.sanitizedIconSvg = this.sanitizer.bypassSecurityTrustHtml(this.iconSvg);
+  }
+  
+  getGradientBackground(): string {
+    return `linear-gradient(to right, ${this.color1} ${this.color1Percentage}, ${this.color2} ${this.color2Percentage})`;
+  }
 }
