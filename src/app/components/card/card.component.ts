e82b492a2b7b0451ea318c33b127f1db18279585
Jomi Susan Mathew <jomisusanmathew@gmail.com>
2024-06-30 13:27:00 +0530

    update: took data dynamically using input

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,23 @@
-import { Component,ChangeDetectionStrategy } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  @Input() privacyIncidentTotalCount!: number;
+  @Input() privacyIncidentPending!: number;
+  @Input() privacyIncidentClosed!: number;
 
+  @Input() securityIncidentTotalCount!: number;
+  @Input() securityIncidentPending!: number;
+  @Input() securityIncidentClosed!: number;
+
+  @Input() qualityIncidentTotalCount!: number;
+  @Input() qualityIncidentPending!: number;
+  @Input() qualityIncidentClosed!: number;
 }
