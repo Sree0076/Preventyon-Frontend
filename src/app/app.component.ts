21ad7277c8ba5dd24378e63b15780ebd4a9dcbb5
NevinJoseph22 <nevinjoseph2001@gmail.com>
2024-07-06 18:08:00 +0530

    bar-chart-with-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet,BarChartComponent]
 })
+
+
+
 export class AppComponent {
   title = 'preventyon';
 }
