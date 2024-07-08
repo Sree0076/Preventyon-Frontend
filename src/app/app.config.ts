37cd8ddb0135a48de7d754e35a4449964cae2e6e
Sreejith Shaji <sreejithshaji0076@gmail.com>
2024-07-08 15:49:11 +0530

    bug fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
@@ -14,7 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
     provideHttpClient(),
   ],
 };
--- src/app/service/tablefetch.service.ts
+++ src/app/service/tablefetch.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IncidentData } from '../components/table/table.component';
 @Injectable({
   providedIn: 'root'
 })
 
 export class TablefetchService {
 
-  private incidentsUrl = 'https://api.jsonsilo.com/public/e0259933-a369-4c73-9475-66315ae36922'; // Adjust URL based on your JSON location
 
   constructor(private http: HttpClient) { }
 
