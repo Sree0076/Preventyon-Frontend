7a0248c76a339620726ce5e3cea7d5037ecf716d
Jomi Susan Mathew <jomisusanmathew@gmail.com>
2024-07-09 15:15:59 +0530

    add: incident report form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { IncidentReportFormComponent } from './components/incident-report-form/incident-report-form.component';
 export const routes: Routes = [
-  {path: 'admin', component:AdminDashboardComponent},
-  {path: 'user', component:UserDashboardComponent},
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'user', component: UserDashboardComponent },
   { path: '', component: DashboardComponent },
+  { path: 'form', component: IncidentReportFormComponent },
 ];
