ad16f0bb97717d39a4bac03f4d9060caaed90b3b
Jomi Susan Mathew <jomisusanmathew@gmail.com>
2024-07-11 23:09:39 +0530

    feat: add view incident

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,15 @@ import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.co
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { IncidentReportFormComponent } from './components/incident-report-form/incident-report-form.component';
 
-import { IncidentViewFormComponent } from './components/incident-view-form/incident-view-form.component';
-
 import { IncidentCreatePageComponent } from './pages/incident-create-page/incident-create-page.component';
+import { ViewIncidentFormComponent } from './components/view-incident-form/view-incident-form.component';
 
 export const routes: Routes = [
   { path: 'admin', component: AdminDashboardComponent },
   { path: 'user', component: UserDashboardComponent },
   { path: '', component: DashboardComponent },
   { path: 'form', component: IncidentReportFormComponent },
-  { path: 'view-incident', component: IncidentViewFormComponent },
+  { path: 'view-incident', component: ViewIncidentFormComponent },
   { path: 'form/:action', component: IncidentCreatePageComponent },
 
 ];
