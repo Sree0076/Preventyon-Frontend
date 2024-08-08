8484196570fa784ceab2f17fe9b141f480185f58
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2024-08-08 10:55:47 +0530

    add: forward form message

--- src/app/services/forward-form.service.ts
+++ src/app/services/forward-form.service.ts
@@ -4,20 +4,28 @@ import { Observable } from 'rxjs';
 import { userDetails } from '../models/users_forward_form.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForwardFormService {
-
   constructor(private http: HttpClient) {}
 
-  getAllUsers():Observable<any>{
-    return this.http.get<any>('http://localhost:7209/api/Employee/GetEmployees');
+  getAllUsers(): Observable<any> {
+    return this.http.get<any>(
+      'http://localhost:7209/api/Employee/GetEmployees'
+    );
   }
 
-  forwardIncident(incidentId: number, assignedEmployeeIds: number[]): Observable<any> {
+  forwardIncident(
+    incidentId: number,
+    assignedEmployeeIds: number[],
+    remarks: string
+  ): Observable<any> {
     console.log(assignedEmployeeIds);
     const url = `http://localhost:7209/api/AssignedIncident/AssignIncidentToEmployees/AssignIncidentToEmployees/${incidentId}`;
-    return this.http.post(url, assignedEmployeeIds);
+    const requestBody = {
+      assignedEmployeeIds: assignedEmployeeIds,
+      remarks: remarks,
+    };
+    return this.http.post(url, requestBody);
   }
-
 }
